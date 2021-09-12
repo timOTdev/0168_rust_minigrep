508b50cd3b1003066bf05761ffa42f35af04af24
Tim H <timh1203@gmail.com>
2021-09-12 10:12:08 -0500

    Split to Config struct and parse_config()

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
 fn main() {
-    // Collect the args into a vector of strings.
+    // ===Collect the args into a vector of strings.
     let args: Vec<String> = env::args().collect();
 
-    // Pull out individual args.
-    // Index 0 is the binary so we skip that.
-    let query = &args[1];
-    let filename = &args[2];
+    // Call the function to parse.
+    let config: Config = parse_config(&args);
 
-    // ==Return all the args.
+    // Return all the args.
     println!("===args: {:?}", args);
     
-    // ==Returns the saved args.
-    println!("===query: {}", query);
+    // Returns the saved args.
+    println!("===query: {}", config.query);
     
     // Print out the filename and contents.
-    println!("===filename: {}", filename);
+    println!("===filename: {}", config.filename);
 
     // This opens the file and returns Result type.
     // Use expect() to handle Ok and Err.
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     // Prints out the contents of the file.
@@ -44,4 +47,17 @@ fn main() {
     // How public, like a frog
     // To tell your name the livelong day
     // To an admiring bog!
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // Pull out individual args.
+    // Index 0 is the binary so we skip that.
+    // We clone the strings here to not take ownership.
+    // Not performant but easiest to do right now.
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    // Make a new config to relate the data.
+    // We returned a tuple before but that doesn't show relationship.
+    Config{query, filename}
 }
